727996fc8c519fae59a469a0221c28377b9ed077
Chabloz <43348620+Chabloz@users.noreply.github.com>
2019-02-24 22:02:57 +0100

    Update JsonStorage.js

--- webmobui/resources/JsonStorage.js
+++ webmobui/resources/JsonStorage.js
@@ -1,5 +1,14 @@
+/** Class representing a LocalStorage handler, with JSON and storage's name management */
 export default class {
 
+  /**
+   * Create a JsonStorage.
+   * 
+   * @param {Object} [options={}] - Options
+   * @param {string} [options.name="default"] - The name of the storage.
+   * @param {boolean} [options.listen=true] - Listen to storage events for data update
+   * @param {boolean} [options.trigger=true] - Trigger an "jsonstorage" event on "window" when data is set, changed or deleted in the storage
+   */  
   constructor(options = {}) {
     this.options = {
       name: options.name || "default",
@@ -7,56 +16,87 @@ export default class {
       trigger: options.trigger || true
     };
     this._reloadKeys();
-    // reload keys when storage change from another tab
+    // reload keys data when storage change from another tab
     if (this.options.listen) {
       window.addEventListener('storage', evt => {
-        this._reloadKeys();
+        this.reloadKeys();
       });
     }
   }
 
+  /**
+   * Private method
+   */
   _reloadKeys() {
     this.storageKeys = Object.keys(localStorage)
       .filter(key => key.startsWith(`${this.options.name}_`))
       .reduce((map, key) => map.set(key.substring(`${this.options.name}_`.length), 1), new Map());
   }
 
+  /**
+   * Returns a new Iterator object that contains an array of [key, value] for each element in the storage
+   */
   [Symbol.iterator]() {
     return this.entries();
   }
 
+  /**
+   * Returns a new Iterator object that contains the keys for each element in the storage.
+   */
   keys() {
     return this.storageKeys.keys();
   }
 
+  /**
+   * Returns the name of the nth key in the storage
+   * 
+   * @param {integer} ind Indice
+   */
   key(ind) {
     let keys = [...this.keys()];
     if (ind < 0 || ind >= keys.length) return null
     return keys[ind];
   }
 
+  /**
+   * Returns a new Iterator object that contains the values for each element in the storage.
+   */
   *values() {
     for (let k of this.storageKeys.keys()) {
       yield this.getItem(k);
     }
   }
 
+  /**
+   * Returns a new Iterator object that contains an array of [key, value] for each element in the storage.
+   */
   *entries() {
     for (let k of this.storageKeys.keys()) {
       yield [k, this.getItem(k)];
     }
   }
 
+  /**
+   * Calls callback once for each key-value pair present in the storage. The callback is called with the value and the key as parameters.
+   * 
+   * @param {function} callback 
+   */
   forEach(callback) {
     for (let entrie of this) {
       callback(entrie[1], entrie[0]);
     }
   }
 
+  /**
+   * Return an array of [key, value] for each element in the storage.
+   */
   toArray() {
     return [...this.entries()];
   }
 
+  /**
+   * Return an object with a propertie key: value for each element in the storage.
+   */
   toObject() {
     let obj = {};
     for (let ent of this) {
@@ -65,47 +105,93 @@ export default class {
     return obj;
   }
 
+  /**
+   * Return a JSON string representing an object with a propertie key: value for each element in the storage.
+   */
   toJSON() {
     return JSON.stringify(this.toObject());
   }
 
+  /**
+   * Return the numbers of elements in the storage
+   */
   get length() {
     return this.storageKeys.size;
   }
 
+  /**
+   * Return the numbers of elements in the storage (alias for this.length)
+   */
   get size() {
     return this.length;
   }
 
+  /**
+   * Add the key and the key's value to the storage, or update that key's value if it already exists
+   * 
+   * @param {string} key the name of the key you want to create/update.
+   * @param {*} val the value you want to give the key you are creating/updating.
+   */
   setItem(key, val) {
     localStorage.setItem(`${this.options.name}_${key}`, JSON.stringify(val));
     this.storageKeys.set(key, 1);
+    if (this.options.trigger) {
+      window.dispatchEvent(new Event("jsonstorage"));
+    }
   }
 
+  /**
+   * Private method
+   */
   _genKey() {
     return 'uid_' + Math.random().toString(36).substring(2, 10);
   }
 
+  /**
+   * Add the value with a random unique key to the storage
+   * 
+   * @param {*} val the value you want to give the key you are creating/updating.
+   */
   addItem(val) {
     let key = this._genKey();
     while (this.storageKeys.has(key)) key = this._genKey();
-    this.setItem(key, val);
+    this.setItem(key, val);    
   }
 
+  /**
+   * Return the value of the key in the storage. If the key does not exist, null is returned.
+   * 
+   * @param {string} key the name of the key you want to retrieve the value of.
+   */
   getItem(key) {
     let val = localStorage.getItem(`${this.options.name}_${key}`);
     if (val == null) return null;
     return JSON.parse(val);
   }
 
+  /**
+   * Remove the key from the storage if it exists. If there is no item associated with the given key, this method will do nothing.
+   * 
+   * @param {string} the name of the key you want to remove. 
+   */
   removeItem(key) {
     localStorage.removeItem(`${this.options.name}_${key}`);
     this.storageKeys.delete(key);
+    if (this.options.trigger) {
+      window.dispatchEvent(new Event("jsonstorage"));
+    }
   }
 
+  /**
+   * Clears all keys in the storage
+   */
   clear() {
     for (let key of this.keys()) {
-      this.removeItem(key);
+      localStorage.removeItem(`${this.options.name}_${key}`);    
+    }
+    this.storageKeys.clear();
+    if (this.options.trigger) {
+      window.dispatchEvent(new Event("jsonstorage"));
     }
   }
 
