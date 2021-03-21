2aede27ea5290173ef896206f6efc8556ab5de42
Nicolas Chabloz <43348620+Chabloz@users.noreply.github.com>
2021-03-21 17:54:39 +0100

    Create workerPreCache.js

---
+++ webmobui/workerPreCache.js
@@ -0,0 +1,33 @@
+const CACHE_VERSION = '1';
+const CACHE_FILES = [
+
+];
+
+self.addEventListener('install', event => {
+  self.skipWaiting();
+  const preCache = async () => {
+    const cache = await caches.open(CACHE_VERSION);
+    return cache.addAll(CACHE_FILES);
+  };
+  event.waitUntil(preCache());
+});
+
+
+self.addEventListener('fetch', event => {
+  const fetchCacheFirst = async () => {
+    const cache = await caches.open(CACHE_VERSION);
+    const cached = await cache.match(event.request);
+    if (cached) return cached;
+    return fetch(event.request);
+  };
+  event.respondWith(fetchCacheFirst());
+});
+
+self.addEventListener('activate', event => {
+  const clearOldCache = async () => {
+    let keys = await caches.keys();
+    keys = keys.filter(key => key !== CACHE_VERSION);
+    return Promise.all(keys.map(key => caches.delete(key)));
+  }
+  event.waitUntil(clearOldCache());
+});
